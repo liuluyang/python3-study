2caa99fc2d5e334d04d27ca5c04a1f15d714ea2d
liuluyang <1120773382@qq.com>
2017-11-08 15:38:14 +0800

    test_1

--- test/test_1.py
+++ test/test_1.py
@@ -1,1 +1,25 @@
-import builtins;builtins.abs=10
+
+
+def money(base,rate):
+    m = base
+    while True:
+        m = m + m*rate
+        yield m
+
+
+m = money(150000, 0.12)
+#m.send(None)
+for i in range(10):
+    print (next(m))
+
+def money_2(base, rate):
+    m = base
+    while True:
+        m = m + m*rate + base
+        yield m
+
+m2 = money_2(50000, 0.1)
+#for y in range(10):
+    #print (next(m2))
+for x,y in enumerate(range(10)):
+    print (x+1,'年', next(m2))
